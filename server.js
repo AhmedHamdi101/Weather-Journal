20b361f3d4694fdcd7f9757a152f9728c2b2502a
Ahmed Hamdi <ahmed.moh.hamdi@gmail.com>
2021-04-03 15:31:02 +0200

    server is done without the api

--- server.js
+++ server.js
@@ -2,18 +2,42 @@
 projectData = {};
 
 // Require Express to run server and routes
+const express = require('express');
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
+const bodyParser = require('body-parser');
+
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
+
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
 // Setup Server
+const port = 8000;
+const server = app.listen(port,listening);
+
+function listening(){
+    console.log("The server is runing... ");
+    console.log("The server is on PORT : ",port);
+}
+
+
+
+const data = [];
+app.post('/addMovie',postfunction);
+
+function postfunction(req,res){
+    data.push(req.body);
+    console.log(data);
+}
